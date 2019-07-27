40e7fadb0066b573208c3ec397330ab4b90648eb
John C. Kennedy <devjckennedy@gmail.com>
2019-07-27 23:10:37 +0900

    setup webpack for client

---
+++ webpack.config.babel.js
@@ -0,0 +1,89 @@
+import webpack from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+import {
+  getDevTool, getEntry, getEnvPlugins, getFileName, getOptimisers, resolvePath
+} from './webpackHelpers';
+
+export default (_, argv) => ({
+  devtool: getDevTool(argv),
+  entry: getEntry(argv),
+  externals: {
+    'react/addons': 'react',
+    'react/lib/ExecutionEnvironment': 'react',
+    'react/lib/ReactContext': 'react'
+  },
+  output: {
+    path: resolvePath('client/assets'),
+    filename: getFileName(argv),
+    chunkFilename: getFileName(argv),
+    publicPath: '/'
+  },
+  mode: argv.mode,
+  module: {
+    rules: [
+      {
+        test: /(\.js$)/,
+        include: resolvePath('client/src'),
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-react', '@babel/preset-env']
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+      },
+      {
+        test: /\.(gif|jpe?g|png|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: 'client/assets/images/[name].[hash:4].[ext]'
+          }
+        }
+      },
+      {
+        test: /\.(eot|ttf|woff2?)(\?v=\d+\.\d+\.\d+)?$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: 'client/assets/fonts/[name].[hash:4].[ext]'
+          }
+        }
+      },
+      {
+        test: /\.(aac|flac|mkv|mp3|mp4|ogg|wma|wmv)(\?v=\d+\.\d+\.\d+)?$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: 'client/assets/media/[name].[hash:4].[ext]'
+          }
+        }
+      }
+    ]
+  },
+  ...getOptimisers(argv),
+  resolve: {
+    extensions: ['.js', '.json', '.jsx'],
+    modules: [__dirname, 'src', 'node_modules'],
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash].css',
+      chunkFilename: '[id].[hash].css'
+    }),
+    ...getEnvPlugins(argv)
+  ]
+});
---
+++ webpackHelpers.js
@@ -0,0 +1,75 @@
+import path from 'path';
+import webpack from 'webpack';
+
+export const DEVELOPMENT_ENV = 'development';
+export const PRODUCTION_ENV = 'production';
+
+export const resolvePath = dir => path.resolve(__dirname, dir);
+export const getDevTool = ({ mode }) => (mode === DEVELOPMENT_ENV ? 'cheap-module-eval-sourcemap' : 'source-map');
+
+export const getEntry = ({ mode }) => {
+  if (mode === DEVELOPMENT_ENV) {
+    return [
+      './client/src/index.js'
+    ];
+  }
+  return {
+    main: './client/src/index.js'
+  };
+};
+
+export const getEnvPlugins = ({ mode }) => {
+  if (mode === PRODUCTION_ENV) {
+    return [
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify(PRODUCTION_ENV)
+        }
+      })
+    ];
+  }
+  // Excluding hot module in production fixes
+  // EventSource's response has a MIME type ('text/html')
+  // that is not 'text/event-stream'. Aborting the connection. error
+  return [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(DEVELOPMENT_ENV)
+      }
+    })
+  ];
+};
+
+export const getFileName = ({ mode }) => {
+  if (mode === DEVELOPMENT_ENV) {
+    return '[name].[hash].js';
+  }
+  return '[name].[chunkhash].js';
+};
+
+export const getOptimisers = ({ mode }) => {
+  if (mode !== PRODUCTION_ENV) {
+    return {
+      optimization: {
+        minimize: false
+      }
+    };
+  }
+
+  return {
+    optimization: {
+      minimize: true,
+      runtimeChunk: false,
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
+          },
+        },
+      }
+    }
+  };
+};
