3fdb77fd304a3befc67876a42a10dcc0ce57b541
John C. Kennedy <devjckennedy@gmail.com>
2019-07-28 09:49:26 +0900

    configure react router, server and client

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,36 @@
 import React from 'react';
-import Home from './Home';
+import { Route, Switch, NavLink } from 'react-router-dom';
 
-export default () => {
-  return <Home name="React App" />;
+import Home from './Home';
+import Posts from './Posts';
+import Todos from './Todos';
+import NotFound from './NotFound';
+
+export default props => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/todos">Todos</NavLink>
+        </li>
+        <li>
+          <NavLink to="/posts">Posts</NavLink>
+        </li>
+      </ul>
+
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={props => <Home name="React App" {...props} />}
+        />
+        <Route path="/todos" component={Todos} />
+        <Route path="/posts" component={Posts} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  );
 };
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import path from 'path';
 import React from 'react';
 import express from 'express';
 import ReactDOMServer from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom';
 
 import App from '../client/src/components/App';
 
@@ -13,13 +14,27 @@ const app = express();
 app.use(express.static('./client/assets'));
 
 app.get('*', (req, res) => {
-  const reactApp = ReactDOMServer.renderToString(<App />);
+  const context = {};
+  const reactApp = ReactDOMServer.renderToString(
+    <StaticRouter location={req.url} context={context}>
+      <App />
+    </StaticRouter>
+  );
 
   const indexFile = path.resolve('./build/index.html');
   fs.readFile(indexFile, 'utf8', (err, data) => {
     if (err) {
       console.error('Something went wrong:', err);
       return res.status(500).send('Oops, better luck next time!');
+    }
+
+    // Handle redirects
+    if (context.url) {
+      return res.redirect(301, context.url);
+    }
+
+    if (context.status === 404) {
+      res.status(404);
     }
 
     return res.send(
@@ -29,5 +44,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.info(`\n🚀 Server is listening on port ${PORT}`);
+  console.info(`\n🚀 Server is listening on port ${PORT} 🚀`);
 });
